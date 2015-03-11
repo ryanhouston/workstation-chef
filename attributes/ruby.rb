f44a175f06d25e57aad0617622f637d634c43983
Ryan Houston <ryanhouston83@gmail.com>
2015-03-11 10:29:02 -0400

    Install Ruby 2.2.0
    Overrides a ruby_build attribute to provide missing package
    dependencies.

--- attributes/ruby.rb
+++ attributes/ruby.rb
@@ -1,10 +1,16 @@
 default['rbenv']['user_installs'] = [
   {
     'user'    => ENV['SUDO_USER'] || ENV['USER'],
-    'rubies'  => ['2.1.5'],
-    'global'  => '2.1.5',
+    'rubies'  => ['2.2.0'],
+    'global'  => '2.2.0',
   }
 ]
 
-default['ruby_buld']['updates'] = true
+default['ruby_build']['updates'] = true
+
+# Add missing dependencies from ruby_build cookbook
+# https://github.com/fnichol/chef-ruby_build/issues/31
+override['ruby_build']['install_pkgs_cruby'] =
+  %w( autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev
+      zlib1g-dev libncurses5-dev libffi-dev libgdbm3 libgdbm-dev )
 
